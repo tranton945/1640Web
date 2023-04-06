647b76f33f399386707f964f5f0d9ab6bc91a9d3
tranton945 <tranton945@gmail.com>
2023-04-06 15:51:56 +0700

    download file.csv

--- 1640WebApp/Controllers/SubmissionManageByAdminController.cs
+++ 1640WebApp/Controllers/SubmissionManageByAdminController.cs
@@ -6,9 +6,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Org.BouncyCastle.Crypto;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
 
 
 namespace _1640WebApp.Controllers
@@ -30,7 +35,7 @@ namespace _1640WebApp.Controllers
         }
 
         // downloda zip
-        public async Task<IActionResult> Download(int id) 
+        public async Task<IActionResult> DownloadZip(int id) 
         {
             var ideas = _context.Ideas.Where(i => i.SubmissionId == id).ToList();
 
@@ -62,6 +67,62 @@ namespace _1640WebApp.Controllers
                 var fileName = $"submission_{id}_files.zip";
                 return File(ms.ToArray(), "application/zip", fileName);
             }
+        }
+
+        // CSV
+        public async Task<IActionResult> DownloadCSV(int id)
+        {
+            var ideas = await _context.Ideas
+                        .Where(i => i.SubmissionId == id)
+                        .Select(i => new {
+                            i.Id,
+                            i.Title,
+                            i.Text,
+                            i.FilePath,
+                            i.Datatime,
+                            i.Img,
+                            i.Data,
+                            i.Anonymous,
+                            i.IdeaId,
+                            i.CreatorEmail,
+                            i.FileName,
+                            i.UserId,
+                            i.DepartmentId,
+                            i.ViewCount
+                        })
+                        .ToListAsync();
+
+
+            var CsvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                // Cấu hình các thiết lập cho file CSV
+                // Delimiter nên dùng dấu "," để định dạng các trường là một cột
+                Delimiter = ",",
+                HasHeaderRecord = true,
+                Quote = '"'
+            };
+            using var writer = new StringWriter();
+            using var csv = new CsvWriter(writer, CsvConfig);
+
+            // Ghi dữ liệu cho file CSV
+            csv.WriteRecords(ideas);
+
+            // Ghi header cho file CSV
+            csv.WriteHeader<Idea>();
+            foreach (var idea in ideas)
+            {
+                csv.WriteRecord(idea);
+            }
+            var sub = await _context.Submissions.FirstOrDefaultAsync(o => o.Id == id);
+            var Name = sub?.Name + ".csv";
+            // dùng Replace để đổi các khoảng trắng " " thành "_"
+            var fileName = Name.Replace(" ", "_");
+            if (Name == null)
+            {
+                return File(new MemoryStream(writer.Encoding.GetBytes(writer.ToString())), "text/csv", "ideas.csv");
+            }
+            // Trả về file CSV
+            return File(new MemoryStream(writer.Encoding.GetBytes(writer.ToString())), "text/csv", fileName);
         }
 
 
